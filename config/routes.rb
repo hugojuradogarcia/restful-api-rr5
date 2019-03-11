505c172c1129478b7db6f2f15ad6122ceb6e81a9
hugojuradoconekta <hugo.jurado@conekta.com>
2019-03-11 17:42:01 -0600

    Fix error in routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,4 @@ Rails.application.routes.draw do
   end  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   constraints subdomain: 'api' do
   end
-e
+end
