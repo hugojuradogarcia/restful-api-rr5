40aace7ee1af778bb12e2712eff4b3f650ac4382
hugojuradoconekta <hugo.jurado@conekta.com>
2019-03-12 11:04:28 -0600

    Action Update

--- app/controllers/v1/books_controller.rb
+++ app/controllers/v1/books_controller.rb
@@ -19,6 +19,16 @@ class V1::BooksController < ApplicationController
     render json: book, status: :ok
   end
 
+  def update
+    book = Book.find(params[:id])
+
+    if book.update(book_params)
+      render json: book, status: :ok
+    else
+      render json: { errors: book.errors },status: :unprocessable_entity
+    end
+  end
+
   private
 
   def book_params
