6907731c80e0e2b3e54045726b86b311b3ed7909
hugojuradoconekta <hugo.jurado@conekta.com>
2019-03-12 10:55:42 -0600

    Action create

--- app/controllers/v1/books_controller.rb
+++ app/controllers/v1/books_controller.rb
@@ -3,4 +3,20 @@ class V1::BooksController < ApplicationController
     books = Book.all
     render json: books, status: :OK
   end
+
+  def create
+    book = Book.new(book_params)
+
+    if book.save
+      render json: book, status: :created
+    else
+      render json: { errors: book.errors }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :weight, :publisher, :language, :pages, :publication_date)
+  end
 end
